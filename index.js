1bdbd76f284da692b9b95d225ec4f848e83188e8
Jonathan Powell <jprophit@gmail.com>
2025-01-13 09:32:59 +0000

    first home route created

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-console.log("Hello Codespaces!");
+const app = require('./app');
+const {PORT} = process.env
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
